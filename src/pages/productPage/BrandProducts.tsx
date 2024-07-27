64941c61a56e16574567aa000f7c488d75c80ff5
sumonpaul55 <sumpaul100@gmail.com>
2024-07-27 18:31:40 +0600

    product displayed brand wise and add to cart and view details

--- src/pages/productPage/BrandProducts.tsx
+++ src/pages/productPage/BrandProducts.tsx
@@ -1,29 +1,60 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import Section from "../../layouts/Section"
-import { useGetAllProductQuery } from "../../redux/features/products/productApi";
-import Loading from "../commonPage/Loading";
+import { useAddToCartMutation, useGetAllProductQuery } from "../../redux/features/products/productApi";
+import Loading, { AnimateSpin } from "../commonPage/Loading";
 import { TProduct } from "../../components/FeaturedProduct/FeaturedProduct";
+import { motion } from "framer-motion";
+import { Button } from "antd";
+import { toast } from "sonner";
 
 const BrandProducts = () => {
     const brand = useParams();
+    const [addCart] = useAddToCartMutation()
     const { data, isLoading } = useGetAllProductQuery(brand)
     const brandData = data?.data
     if (isLoading) {
         return <Loading></Loading>
     }
+    // handle add to cart
+    const handleAddtoCart = (productId: any) => {
+        if (productId) {
+            const cartProduct = {
+                productId: productId,
+                quantity: 1
+            }
+            addCart(cartProduct)
+            toast.success("This Product Added to Your Cart", {
+                duration: 4000,
+            })
+        }
+    }
     return (
         <Section className="min-h-screen">
             <h1 className="sm:text-lg md:text-xl lg:text-2xl text-center font-bold">All products of <span className="text-primary">{brand.brand}</span> Brands</h1>
-            <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10">
+            <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10 lg:gap-12">
                 {
                     brandData?.map((items: TProduct, idx: number) => (
-                        <div key={idx}>
-                            <img src={items?.image} alt="" />
-                        </div>
+                        <motion.div key={idx} className="border box shadow" whileHover={{ scale: [null, 1, 1.08] }}
+                            transition={{ duration: 0.3 }}>
+                            <div className="relative">
+                                <img src={items?.image} alt={items.name} />
+                                <div className="absolute top-1 right-1 py-2 px-3 bg-red-600 text-white">{items.brand}</div>
+                            </div>
+                            <div className="p-3 md:p-4">
+                                <div className="flex items-center justify-between">
+                                    <h3 className="font-semibold md:text-lg">{items.name}</h3>
+                                    <Button onClick={() => handleAddtoCart(items._id)} style={{ padding: "0 5px" }}>Add to cart</Button>
+                                </div>
+                                <p className="mt-4 line-clamp-2">{items.description}</p>
+                                <Link to={`/product-details/${items?._id}`}>
+                                    <Button style={{ width: "100%" }} className="mt-4 bg-base-yellow text-white flex items-center gap-3">View Details</Button>
+                                </Link>
+                            </div>
+                        </motion.div>
                     ))
                 }
             </div>
-        </Section>
+        </Section >
     )
 }
 
