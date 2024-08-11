1f148ea48044165843a51421d7dc3cf5b0a3d74f
sumonpaul55 <sumpaul100@gmail.com>
2024-08-12 02:01:14 +0600

    scroll top top when navigate page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@
 import './App.css'
 import Mainlayout from './layouts/Mainlayout'
 import Gotop from './pages/commonPage/Gotop'
+import ScrollToTop from './pages/commonPage/ScrollToTop'
 
 function App() {
 
   return (
     <>
+      <ScrollToTop />
       <Mainlayout></Mainlayout>
-
       <Gotop />
     </>
   )
